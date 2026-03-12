096e46856085e301e1952e6f7f86b74175c0fedd
Luciano Arruda <lucasards32@gmail.com>
2026-03-11 22:01:55 -0300

    Criação da Main personalizada

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,111 @@
 package org.example;
 
-import org.example.dominio.Conteudo;
-import org.example.dominio.Curso;
-import org.example.dominio.Dev;
-import org.example.dominio.Mentoria;
+import org.example.dominio.*;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Curso curso1 = new Curso();
-        curso1.setDescricao("logica");
-        curso1.setTitulo("logica");
-        curso1.setCargaHoraria(2);
+        Scanner sc = new Scanner(System.in);
 
+        // ============ CRIAÇÃO DE CONTEÚDOS ==============
 
-        System.out.println(curso1);
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Java");
+        cursoJava.setDescricao("Curso de Java POO");
+        cursoJava.setCargaHoraria(12);
 
-        Mentoria mentoria1 = new Mentoria();
-        mentoria1.setDescricao("Mentoria java");
-        mentoria1.setTitulo("java");
-        mentoria1.setData(LocalDate.now());
+        Curso cursoPython = new Curso();
+        cursoPython.setTitulo("Python");
+        cursoPython.setDescricao("Curso de python");
+        cursoPython.setCargaHoraria(8);
 
-        Conteudo conteudo = new Curso();
-        conteudo.setDescricao("curso de python");
-        conteudo.setTitulo("Python");
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("mentoria Java");
+        mentoriaJava.setDescricao("Tira dúvidas sobre a linguagem Java");
+        mentoriaJava.setData(LocalDate.of(2026,04,10));
 
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("BootCamp Java");
+        bootCamp.setDescricao("Aprenda boas praticas em java");
 
+        bootCamp.getConteudos().add(cursoJava);
+        bootCamp.getConteudos().add(cursoPython);
+        bootCamp.getConteudos().add(mentoriaJava);
 
-        System.out.println(conteudo);
+        // =======CRIANDO DEV =========
 
-        System.out.println(mentoria1);
+        Dev dev = new Dev();
+
+        int opcao = -1;
+
+        while (opcao != 0){
+
+            System.out.println("===== MENU BOOTCAMP ======");
+            System.out.println("1 - Criar Dev");
+            System.out.println("2 - Inscrever Dev no bootcamp");
+            System.out.println("3 - Progredir no curso");
+            System.out.println("4 - Ver conteúdos inscritos");
+            System.out.println("5 - Ver conteúdos concluídos");
+            System.out.println("6 - Ver XP total");
+            System.out.println("7 - Ver Devs inscritos no Bootcamp");
+            System.out.println("0 - Sair");
+
+            System.out.println("Escolha uma opção");
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao){
+                case 1:
+                    System.out.println("Digite o nome do Dev: ");
+                    dev.setNome(sc.nextLine());
+
+                    System.out.println("Dev Criado com sucesso!");
+                    break;
+
+                case 2:
+                    dev.inscreverBootCamp(bootCamp);
+                    System.out.println("Dev inscrito no Bootcamp!");
+                    break;
+
+                case 3:
+                    dev.progredir();
+                    System.out.println("Progresso realizado!");
+                    break;
+
+                case 4:
+                    System.out.println("\nConteúdos inscritoss");
+                    dev.getConteudosInscritos().forEach(System.out::println);
+                    break;
+
+                case 5:
+                    System.out.println("\nConteúdos concluídos");
+                    dev.getConteudosConcluidos().forEach(System.out::println);
+                    break;
+
+                case 6:
+                    System.out.println("Pontuação do Dev");
+                    System.out.println("\nXP Total: " + dev.calcularTotalXp());
+                    break;
+
+                case 7:
+                    System.out.println("Número de Devs inscritos: ");
+                    bootCamp.getDevInscrito().forEach(d -> System.out.println(d.getNome()));
+                    break;
+
+                case 0:
+                    System.out.println("Platform sendo encerrado...");
+
+                default:
+                    System.out.println("Opção inválida");
+
+            }
+
+        }
+        sc.close();
 
         }
     }
